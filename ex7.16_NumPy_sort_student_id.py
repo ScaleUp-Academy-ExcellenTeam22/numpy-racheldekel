d1008c026f50efec062ddc3f4178b8b21462f945
dekel <dekelrachel@gmail.com>
2022-05-15 14:20:52 +0300

    ex7.16_NumPy_sort_student_id

--- ex7.16_NumPy_sort_student_id.py
+++ ex7.16_NumPy_sort_student_id.py
@@ -1,25 +1,18 @@
 import numpy as np
-import nptyping
 
 
-def sort_by_student_id_and_height(student_id: nptyping.ndarray,
-                                  student_height: nptyping.ndarray):
+def sort_student_id():
     """
-    function that compute the median of flattened given array.
-    Note: First array elements raised to powers from second array
-    :param student_id:
-    :param student_height:
+    function sort the student id with increasing height of the students from given students id and height.
+    Print the integer indices that describes the sort order by multiple columns and the sorted data.
     :return:
     """
-    return np.lexsort((student_id, student_height))
+    student_id_array = np.array([1, 2, 3, 4, 5, 6, 7])
+    student_height_array = np.array([80., 85., 85., 81., 88., 84., 82.0])
+    sort_height = np.lexsort((student_id_array, student_height_array))
+    for element in sort_height:
+        print(student_id_array[element], student_height_array[element])
 
 
 if __name__ == '__main__':
-    student_id_array = np.array([1023, 5202, 6230, 1671, 1682, 5241, 4532])
-    student_height_array = np.array([40., 42., 45., 41., 38., 40., 42.0])
-    indices = sort_by_student_id_and_height(student_id_array, student_height_array)
-    print("Sorted indices:")
-    print(indices)
-    print("Sorted data:")
-    for n in indices:
-        print(student_id_array[n], student_height_array[n])
+    sort_student_id()
